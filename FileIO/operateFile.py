2e42584c829878f3f3d91a02c8fb8c24773c3e74
BangLong Liu <1255877369@qq.com>
2019-07-08 07:04:53 +0800

    Update operateFile.py

--- FileIO/operateFile.py
+++ FileIO/operateFile.py
@@ -8,30 +8,40 @@ def openPath(path=""):
     for filename in fileList:
         pathTmp = os.path.join(path,filename)   #获取path与filename组合后的路径
         if os.path.isdir(pathTmp):   #如果是目录
+            # pass
             openPath(pathTmp)        #则递归查找
             '''
             目录更名
             '''
             # print(filename)
-            out = getNumber(filename)
-            newPath = os.path.join(path,str(out))
-            os.renames(pathTmp,newPath)
+            # out = rmAD(filename)
+            # newPath = os.path.join(path,str(out))
+            # os.renames(pathTmp,newPath)
             # out = getTen(pathTmp[-1:])
         #     if out is not 10:
         #         newPath = os.path.join(path,str(out))
         #         os.renames(pathTmp,newPath)
         #     print(pathTmp)
-        
+
         else: # 文件
-            pass
+            # pass
             # print("file: "+ pathTmp)
-            # print("name:"+filename)
+            # print("name: "+filename)
+            # exten = os.path.splitext(filename)   # 取文件 后缀
+            # if exten[1] == '.txt':
+            #     new = exten[0]
+            #     newPath = os.path.join(path, new)
+            #     # print(pathTmp,newPath)
+            #     os.renames(pathTmp,newPath)
             # new = getNumber(filename)
-            # newPath = os.path.join(path, new)
-            # os.renames(pathTmp,newPath)
+            # new = addNum(filename)      # 个位数字后加点
+            # new = addDot(filename)          # 数字后加点
+            new = rmAD(filename)
+            newPath = os.path.join(path, new)
+            os.renames(pathTmp,newPath)
             # print(newPath)
             # print("new"+new)
-            # new = re.sub('_recv','',filename)
+            # # new = re.sub('_recv','',filename)
             # newPath = os.path.join(path, new)
             # os.renames(pathTmp,newPath)
 
@@ -54,7 +64,8 @@ def main():
     # for path in lis:
     #     openPath(path)
     # path = r'F:\多啦A梦[xxxx.xxx]'
-    path = r'E:\《算法很美》'
+    # path = r'E:\21 git安装和使用'
+    path = r'E:\08. 超全技术电子书汇总'
     openPath(path)
 
 
