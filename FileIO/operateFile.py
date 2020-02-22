fcd437eaa84ce0f25381481fb71ba202a0a7d5f9
邦龙 刘 <1255877369@qq.com>
2020-02-22 08:19:39 +0800

    .

--- FileIO/operateFile.py
+++ FileIO/operateFile.py
@@ -13,11 +13,11 @@ def openPath(path=""):
             '''
             目录更名
             '''
-            print(filename)
+            # print(filename)
             # out = rmAD(filename) 
-            out = getNumber(filename)  # 中文数字转英文
-            newPath = os.path.join(path,str(out))
-            os.renames(pathTmp,newPath)
+            # out = getNumber(filename)  # 中文数字转英文
+            # newPath = os.path.join(path,str(out))
+            # os.renames(pathTmp,newPath)
             # out = getTen(pathTmp[-1:])
         #     if out is not 10:
         #         newPath = os.path.join(path,str(out))
@@ -37,33 +37,27 @@ def openPath(path=""):
             # new = getNumber(filename)
             # new = addNum(filename)      # 个位数字后加点
             # new = addDot(filename)          # 数字后加点
-            # new = rmAD(filename)
-            # newPath = os.path.join(path, new)
-            # os.renames(pathTmp,newPath)
+            new = rmAD(filename)				#  删除【】广告
+            newPath = os.path.join(path, new)
+            os.renames(pathTmp,newPath)
             # print(newPath)
             # print("new"+new)
             # # new = re.sub('_recv','',filename)
             # newPath = os.path.join(path, new)
             # os.renames(pathTmp,newPath)
 
-            '''
-            # out = rmAD(filename)
-            out = getNumber(filename)
-            print(out)
-            if out is not None:
-                newPath = os.path.join(path,out)
-                os.renames(pathTmp,newPath)
-
-            # newPath = os.path.join(path, rmAD(filename))
-            # os.renames(filename,newPath)
-            '''
-
 def main():
     # path=r"C:\Users\Pancras\Desktop\课件"
     # lis = [s+str(i) for i in range(2,52)]
     # for path in lis:
     #     openPath(path)
-    path = ''
+    path = r'E:\Java'
+    path=r'E:\【瑞客论坛 www.ruike1.com】CTF夺旗训练视频课程'
+    path=r'E:\【瑞客论坛 www.ruike1.com】百知教育 - 面向对象的基础'
+    path=r'E:\【瑞客论坛 www.ruike1.com】2020年最新 深圳Java190722班技术简历辅导'
+    path=r'E:\【瑞客论坛 www.ruike1.com】【天勤】2021天勤计算机全程班【现售：￥1980】'
+    path=r'E:\【瑞客论坛 www.ruike1.com】黑马头条项目 - Java Springboot2.0（资料、代码。讲义）14天完整'
+    path=r'E:\【瑞客论坛 www.ruike1.com】spring源码底层分析'
     openPath(path)
 
 
