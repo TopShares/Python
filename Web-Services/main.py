5318fde1f85025788e48c078d3aa0b208acbe7b2
TopShares <1255877369@qq.com>
2020-03-22 23:07:33 +0800

    .

--- Web-Services/main.py
+++ Web-Services/main.py
@@ -2,7 +2,7 @@ import sys
 from spyne import Application,rpc,ServiceBase,Iterable,Integer,Unicode
 from spyne.protocol.soap import Soap11,Soap12
 from spyne.server.wsgi import WsgiApplication
- 
+
 class HelloWorldService1(ServiceBase):
 
     @rpc(Unicode,Integer,_returns=Iterable(Unicode))
@@ -16,7 +16,7 @@ class HelloWorldService1(ServiceBase):
         for i in range(times):
             yield u'say_hello11 : Hello,%s' % name
     @rpc(Unicode,Integer,_returns=Iterable(Unicode))
- 
+
     def say_hello12(ctx,name,times):
         """Docstrings for service methods appear as documentation in the wsdl.
         <b>What fun!</b>
@@ -26,7 +26,7 @@ class HelloWorldService1(ServiceBase):
         """
         for i in range(times):
             yield u'say_hello12 :　Hello,%s' % name
- 
+
 
     @rpc(Unicode, Unicode, Unicode, _returns=Iterable(Unicode))
     def login(ctx,MAC,UserId,password):
@@ -43,7 +43,7 @@ class HelloWorldService2(ServiceBase):
         """
         for i in range(times):
             yield u'say_hello21 : Hello,%s' % name
- 
+
     @rpc(Unicode,Integer,_returns=Iterable(Unicode))
     def say_hello22(ctx,name,times):
         """Docstrings for service methods appear as documentation in the wsdl.
@@ -54,17 +54,17 @@ class HelloWorldService2(ServiceBase):
         """
         for i in range(times):
             yield u'say_hello22 : Hello,%s' % name
- 
+
 # application = Application([HelloWorldService1,HelloWorldService2], 'http://www.cooldota.cn/mywebservice.asmx?WSDL',in_protocol=Soap11(validator='lxml'),out_protocol=Soap11())
 application = Application([HelloWorldService1,HelloWorldService2],'http://schemas.xmlsoap.org/soap/envelope',in_protocol=Soap11(validator='lxml'),out_protocol=Soap11())
 wsgi_application = WsgiApplication(application)
- 
+
 if __name__ == '__main__':
     import logging
     from wsgiref.simple_server import make_server
     logging.basicConfig(level=logging.DEBUG)
     logging.getLogger('spyne.protocol.xml').setLevel(logging.DEBUG)
-    logging.info("listening to http://192.168.2.135:8080")
-    logging.info("wsdl is at: http://192.168.2.135:8080/?wsdl")
-    server = make_server('192.168.2.135',8080,wsgi_application)
+    logging.info("listening to http://192.168.2.238:8080")
+    logging.info("wsdl is at: http://192.168.2.238:8080/?wsdl")
+    server = make_server('192.168.2.238',8080,wsgi_application)
     sys.exit(server.serve_forever())
